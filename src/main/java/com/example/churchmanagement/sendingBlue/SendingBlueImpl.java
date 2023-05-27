d9a947421d63ba1b16137af582c7ddac55842139
samuelshola777 <samuelshola14@gmail.com>
2023-05-27 01:26:19 +0100

    sending blue function

--- src/main/java/com/example/churchmanagement/sendingBlue/SendingBlueImpl.java
+++ src/main/java/com/example/churchmanagement/sendingBlue/SendingBlueImpl.java
@@ -1,14 +1,32 @@
 package com.example.churchmanagement.sendingBlue;
 
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+
+
+
 
 @Service
-
+@AllArgsConstructor
 public class SendingBlueImpl implements  SendingBlueService{
+private  final MailConfig mailConfig;
 
 
     @Override
     public void sendingBlueMailSender(SendingBlueMailRequest sendingRequest) {
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("api-key", mailConfig.getApiKey());
+        HttpEntity<SendingBlueMailRequest> requestEntity = new HttpEntity<>(sendingRequest, headers);
 
+        var response = restTemplate.postForEntity(mailConfig.getUrl(), requestEntity, String.class);
+
+        //return response.getBody();
     }
 }
