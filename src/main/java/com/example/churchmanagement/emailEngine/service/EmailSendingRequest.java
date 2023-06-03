924ced43e8e2f4d67b62d192858ab39d8313778e
samuelshola777 <samuelshola14@gmail.com>
2023-06-03 05:48:59 +0100

    recreating church email request

--- src/main/java/com/example/churchmanagement/emailEngine/service/EmailSendingRequest.java
+++
@@ -1,7 +0,0 @@
-package com.example.churchmanagement.emailEngine.service;
-
-public class EmailSendingRequest {
-    private String emailReceiver;
-    private String emailHeading;
-    private String emailMassage;
-}
