674cf1a507757770b43703abb23eb8eb741da8a6
samuelshola777 <samuelshola14@gmail.com>
2023-06-09 03:13:08 +0100

    rounding up the forgot password method

--- src/main/java/com/example/churchmanagement/controller/ChurchBranchController.java
+++ src/main/java/com/example/churchmanagement/controller/ChurchBranchController.java
@@ -1,9 +1,12 @@
 package com.example.churchmanagement.controller;
 
 import com.example.churchmanagement.dto.request.ChurchRequest;
+import com.example.churchmanagement.dto.response.ChurchResponse;
 import com.example.churchmanagement.service.ChurchService;
+import lombok.AllArgsConstructor;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,12 +19,12 @@ import java.util.concurrent.CompletableFuture;
 @RestController
 @RequiredArgsConstructor
 public class ChurchBranchController {
-    @NonNull
-    private final ChurchService churchService;
+  @NonNull
+  private final ChurchService churchService;
 
     @PostMapping("create")
-    public ResponseEntity<?> createChurchAccount(@RequestBody ChurchRequest churchRequest){
-        return churchService.registerANewChurchBranch(churchRequest);
+   public ResponseEntity<ChurchResponse> createChurchAccount(ChurchRequest churchRequest){
+        return new ResponseEntity<>(churchService.registerANewChurchBranch(churchRequest), HttpStatus.CREATED)
     }
 
 }
