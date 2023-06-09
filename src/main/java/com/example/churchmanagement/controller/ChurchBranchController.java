1ffac07eff4e0caddeba7a0a6592d590c4b5dfb5
samuelshola777 <samuelshola14@gmail.com>
2023-06-09 07:36:33 +0100

    adding quesry method

--- src/main/java/com/example/churchmanagement/controller/ChurchBranchController.java
+++ src/main/java/com/example/churchmanagement/controller/ChurchBranchController.java
@@ -33,6 +33,13 @@ public class ChurchBranchController {
         churchService.deleteAllChurchBranch();
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
-
+@GetMapping("findByEmailResponse")
+    public ResponseEntity<?> findByEmailResponse(@PathVariable String email){
+        return new ResponseEntity<>(churchService.findByEmailResponse(email),HttpStatus.FOUND);
+}
+@GetMapping("findByEmail")
+    public ResponseEntity<?> findChurchByEmail(@PathVariable String email){
+        return new ResponseEntity<>(churchService.findChurchBranchByEmailAddress(email),HttpStatus.FOUND);
+}
 
 }
