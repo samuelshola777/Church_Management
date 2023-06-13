e278c90c245b0bda38f2ff8a4bda72308e844818
samuelshola777 <samuelshola14@gmail.com>
2023-06-13 07:54:01 +0100

    continue debugin register method

--- src/main/java/com/example/churchmanagement/controller/ChurchBranchController.java
+++ src/main/java/com/example/churchmanagement/controller/ChurchBranchController.java
@@ -33,7 +33,7 @@ public class ChurchBranchController {
     public ResponseEntity<?> findByEmailResponse(@PathVariable String email){
         return new ResponseEntity<>(churchService.findByEmailResponse(email),HttpStatus.FOUND);
 }
-@GetMapping("findByEmail")
+@GetMapping("findByEmail{email}")
     public ResponseEntity<?> findChurchByEmail(@PathVariable String email){
         return new ResponseEntity<>(churchService.findChurchBranchByEmailAddress(email),HttpStatus.FOUND);
 }
