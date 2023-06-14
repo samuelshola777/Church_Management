603d2d366cfa88f38f6e68921c2300a591a7a1be
samuelshola777 <samuelshola14@gmail.com>
2023-06-14 03:35:10 +0100

    creating the church landing html page

---
+++ src/main/java/com/example/churchmanagement/frontEndWork/churchRegistration/RealLandingPageJavaScript.js
@@ -0,0 +1,5 @@
+// JavaScript to toggle light/dark mode
+document.getElementById('toggle-mode').addEventListener('click', function() {
+    document.body.classList.toggle('light-mode');
+    document.body.classList.toggle('dark-mode');
+});
