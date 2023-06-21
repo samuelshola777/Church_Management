6c8d5067f5ca7efcf7c2555d5a3f26c688c62e83
samuelshola777 <samuelshola14@gmail.com>
2023-06-21 16:05:46 +0100

    still working on the registration

--- src/main/java/com/example/churchmanagement/service/EmailAlreadyInUse.java
+++ src/main/java/com/example/churchmanagement/service/EmailAlreadyInUse.java
@@ -2,7 +2,7 @@ package com.example.churchmanagement.service;
 
 public interface EmailAlreadyInUse {
 
+   void pastorEmailAlreadyInUse(String email);
 
-   void churchEmailAlreadyInUse(String email);
 
 }
