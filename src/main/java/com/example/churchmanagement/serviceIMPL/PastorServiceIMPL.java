680c393cca07665eb84de2c61b8d246f7d12234b
samuelshola777 <samuelshola14@gmail.com>
2023-06-22 09:23:06 +0100

    debuging pastor service

--- src/main/java/com/example/churchmanagement/serviceIMPL/PastorServiceIMPL.java
+++ src/main/java/com/example/churchmanagement/serviceIMPL/PastorServiceIMPL.java
@@ -56,7 +56,7 @@ public class PastorServiceIMPL implements PastorService {
 
     PastorTokenZ tokenZ = tokenService.createPastorToken(mappedPastor);
     mappedPastor.setToken(tokenZ.getToken());
-   mappedPastor.g
+   mappedPastor.getListOfToken().add(tokenZ);
     tokenZ.setPastor(pastorRepository.save(mappedPastor));
     //emailService.sendEmail();
     tokenService.saveToken(tokenZ);
