23d1f61b9d04f75f26a0a43c3dd2af63e54f67e5
samuelshola777 <samuelshola14@gmail.com>
2023-06-24 02:20:28 +0100

    debuged the pastor registraion method

--- src/main/java/com/example/churchmanagement/serviceIMPL/PastorServiceIMPL.java
+++ src/main/java/com/example/churchmanagement/serviceIMPL/PastorServiceIMPL.java
@@ -127,9 +127,11 @@ Pastor foundPastor = pastorRepository.findByEmailAddress(mail);
         Pastor foundPastor = findPastorByEmailAddress(mail);
     if (!token.equals(foundPastor.getToken())) throw new TokenException("invalid token");
       foundPastor.setValidationState(ValidationState.INVALID);
-    foundPastor.setChurchBranch(null);
-    foundPastor.setChurchName(null);
-    pastorRepository.save(foundPastor);
+   if (foundPastor.getValidationState() != ValidationState.VALIDATED) {
+       foundPastor.setChurchBranch(null);
+       foundPastor.setChurchName(null);
+       pastorRepository.save(foundPastor);
+   }
     }
 
 
