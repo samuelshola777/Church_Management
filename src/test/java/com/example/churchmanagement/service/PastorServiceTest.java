1bf19549ac008543cc66dff2ef50af3ec9b4aef9
samuelshola777 <samuelshola14@gmail.com>
2023-06-27 17:33:57 +0100

    working on the get all pastor method

--- src/test/java/com/example/churchmanagement/service/PastorServiceTest.java
+++ src/test/java/com/example/churchmanagement/service/PastorServiceTest.java
@@ -169,7 +169,7 @@ assertDoesNotThrow(()->{ pastorService.deletePastorAccountByEmail("pastorAccount
     void testThatWeCanGetAllPastor(){
 
         List<PastorResponse> listOfPastor = pastorService.getAllPastors(1,2);
-        assertFalse(listOfPastor.isEmpty());
+        assertTrue(listOfPastor.isEmpty());
 
     }
 
