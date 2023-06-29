a940e3d2a3d45829936fe3a0ee9331e7da3c32fc
samuelshola777 <samuelshola14@gmail.com>
2023-06-29 17:41:28 +0100

    push

--- src/main/java/com/example/churchmanagement/serviceIMPL/MemberIMPL.java
+++ src/main/java/com/example/churchmanagement/serviceIMPL/MemberIMPL.java
@@ -1,6 +1,11 @@
 package com.example.churchmanagement.serviceIMPL;
 
 import com.example.churchmanagement.service.MemberService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class MemberIMPL implements MemberService {
+
+
+
 }
