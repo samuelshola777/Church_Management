c8b7c11d71695a3e10fdf3ba5f180ee784b0293e
samuelshola777 <samuelshola14@gmail.com>
2023-06-29 20:19:46 +0100

    working on the member service

--- src/test/java/com/example/churchmanagement/service/MemberServiceTest.java
+++ src/test/java/com/example/churchmanagement/service/MemberServiceTest.java
@@ -1,12 +1,18 @@
 package com.example.churchmanagement.service;
 
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
+@RequiredArgsConstructor
 class MemberServiceTest {
+    @NonNull
+    private MemberService memberService;
+
 
 
     @BeforeEach
