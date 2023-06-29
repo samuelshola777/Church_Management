cf692d09c42e8c7b4f130cc7528033720f36b240
samuelshola777 <samuelshola14@gmail.com>
2023-06-29 17:49:18 +0100

    working on the member service

---
+++ src/main/java/com/example/churchmanagement/dto/request/MemberRequest.java
@@ -0,0 +1,24 @@
+package com.example.churchmanagement.dto.request;
+
+import com.example.churchmanagement.data.model.Address;
+import com.example.churchmanagement.data.model.Role;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.OneToOne;
+import lombok.Data;
+
+@Data
+public class MemberRequest {
+    private  String firstName , lastName;
+
+    private String phoneNumber;
+
+    private String emailAddress;
+
+    private String profile_picture;
+
+    @OneToOne( cascade = CascadeType.ALL, orphanRemoval = true)
+    private Address address;
+
+    private Role role = Role.MEMBER;
+    private String occupation;
+}
