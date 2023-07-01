fd3fefb08b0dd97f7d704534fab27e47b4f0d191
samuelshola777 <samuelshola14@gmail.com>
2023-07-01 11:17:38 +0100

    auth

--- src/main/java/com/example/churchmanagement/facebook/springBoot/controller/FaceBookController.java
+++ src/main/java/com/example/churchmanagement/facebook/springBoot/controller/FaceBookController.java
@@ -2,6 +2,8 @@ package com.example.churchmanagement.facebook.springBoot.controller;
 
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +12,8 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.security.Principal;
+import java.util.Map;
 
 @RestController
 @RequestMapping("api/facebook/")
@@ -24,7 +28,11 @@ public class FaceBookController {
     }
 
 @GetMapping("auth")
-    public String testController(){
+    public String testController(Principal principal){
+    Map<String, Object> authDetails = (Map<String, Object>) ((OAuth2Authentication)principal)
+            .getUserAuthentication()
+            .getDetails();
+    Object key;
 
         return "my name is samuel shola";
     }
